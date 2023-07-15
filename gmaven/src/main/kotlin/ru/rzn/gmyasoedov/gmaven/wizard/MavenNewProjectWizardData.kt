dad933ebdca737bb93ea5ee0be55bd7b60163ea7
gmyasoedov <gmyasoedov@rtc.usetech.ru>
2023-07-15 10:38:02 +0300

    cleanup code

--- gmaven/src/main/kotlin/ru/rzn/gmyasoedov/gmaven/wizard/MavenNewProjectWizardData.kt
+++ gmaven/src/main/kotlin/ru/rzn/gmyasoedov/gmaven/wizard/MavenNewProjectWizardData.kt
@@ -1,4 +1,3 @@
-// Copyright 2000-2023 JetBrains s.r.o. and contributors. Use of this source code is governed by the Apache 2.0 license.
 package ru.rzn.gmyasoedov.gmaven.wizard
 
 import com.intellij.openapi.externalSystem.service.project.wizard.MavenizedNewProjectWizardData
