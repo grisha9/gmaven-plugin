36ec652333d6c32a95bdc6a576826b8b54226b21
gmyasoedov <gmyasoedov@rtc.usetech.ru>
2023-07-21 12:30:31 +0300

    tasks cancelation

--- gmaven/src/main/kotlin/ru/rzn/gmyasoedov/gmaven/project/task/MavenTaskManager.kt
+++ gmaven/src/main/kotlin/ru/rzn/gmyasoedov/gmaven/project/task/MavenTaskManager.kt
@@ -7,12 +7,15 @@ import com.intellij.openapi.externalSystem.service.execution.ExternalSystemJdkUt
 import com.intellij.openapi.externalSystem.service.execution.ProjectJdkNotFoundException
 import com.intellij.openapi.externalSystem.task.ExternalSystemTaskManager
 import ru.rzn.gmyasoedov.gmaven.project.getMavenHome
+import ru.rzn.gmyasoedov.gmaven.server.GServerRemoteProcessSupport
 import ru.rzn.gmyasoedov.gmaven.server.GServerRequest
 import ru.rzn.gmyasoedov.gmaven.server.runTasks
 import ru.rzn.gmyasoedov.gmaven.settings.MavenExecutionSettings
 import java.nio.file.Path
+import java.util.concurrent.ConcurrentHashMap
 
 class MavenTaskManager : ExternalSystemTaskManager<MavenExecutionSettings> {
+    private val cancellationMap = ConcurrentHashMap<ExternalSystemTaskId, GServerRemoteProcessSupport>()
 
     override fun executeTasks(
         id: ExternalSystemTaskId,
@@ -29,19 +32,24 @@ class MavenTaskManager : ExternalSystemTaskManager<MavenExecutionSettings> {
 
         val projectBuildFile = settings.projectBuildFile ?: throw ExternalSystemException("project build file is empty")
         val subProjectBuildFile = settings.subProjectBuildFile
-        if (subProjectBuildFile == null) {
-            val buildPath = Path.of(projectBuildFile)
-            val request = GServerRequest(id, buildPath, mavenHome, sdk, settings, listener = listener)
-            runTasks(request, taskNames, null);
-        } else {
-            val buildPath = if (settings.executionWorkspace.artifactGA == null)
-                Path.of(subProjectBuildFile) else Path.of(projectBuildFile)
-            val request = GServerRequest(id, buildPath, mavenHome, sdk, settings, listener = listener)
-            runTasks(request, taskNames, settings.executionWorkspace.artifactGA);
+        try {
+            if (subProjectBuildFile == null) {
+                val buildPath = Path.of(projectBuildFile)
+                val request = GServerRequest(id, buildPath, mavenHome, sdk, settings, listener = listener)
+                runTasks(request, taskNames, null) { cancellationMap[id] = it }
+            } else {
+                val buildPath = if (settings.executionWorkspace.artifactGA == null)
+                    Path.of(subProjectBuildFile) else Path.of(projectBuildFile)
+                val request = GServerRequest(id, buildPath, mavenHome, sdk, settings, listener = listener)
+                runTasks(request, taskNames, settings.executionWorkspace.artifactGA) { cancellationMap[id] = it }
+            }
+        } finally {
+            cancellationMap.remove(id)
         }
     }
 
     override fun cancelTask(id: ExternalSystemTaskId, listener: ExternalSystemTaskNotificationListener): Boolean {
-        TODO("Not yet implemented")
+        cancellationMap[id]?.stopAll()
+        return true
     }
 }
