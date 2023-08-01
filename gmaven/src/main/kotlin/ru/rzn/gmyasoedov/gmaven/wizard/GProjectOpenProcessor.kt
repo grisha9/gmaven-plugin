966118877e8a192a915286a77877b80a89a0ceb0
gmyasoedov <gmyasoedov@rtc.usetech.ru>
2023-08-01 18:33:30 +0300

    fixed use internal deprecated api

--- gmaven/src/main/kotlin/ru/rzn/gmyasoedov/gmaven/wizard/GProjectOpenProcessor.kt
+++ gmaven/src/main/kotlin/ru/rzn/gmyasoedov/gmaven/wizard/GProjectOpenProcessor.kt
@@ -1,30 +1,35 @@
 package ru.rzn.gmyasoedov.gmaven.wizard
 
-import com.intellij.ide.impl.runUnderModalProgressIfIsEdt
+import com.intellij.openapi.progress.ModalTaskOwner
+import com.intellij.openapi.progress.runBlockingModal
 import com.intellij.openapi.project.Project
 import com.intellij.openapi.vfs.VirtualFile
 import com.intellij.projectImport.ProjectOpenProcessor
 import icons.OpenapiIcons
 import ru.rzn.gmyasoedov.gmaven.GMavenConstants
-import javax.swing.Icon
 
 internal class GProjectOpenProcessor : ProjectOpenProcessor() {
-  private val importProvider = GOpenProjectProvider()
+    private val importProvider = GOpenProjectProvider()
 
+    override fun canOpenProject(file: VirtualFile): Boolean = importProvider.canOpenProject(file)
 
-  override fun canOpenProject(file: VirtualFile): Boolean = importProvider.canOpenProject(file)
+    override fun doOpenProject(
+        virtualFile: VirtualFile,
+        projectToClose: Project?,
+        forceOpenInNewFrame: Boolean
+    ): Project? {
+        return runBlockingModal(ModalTaskOwner.guess(), "") {
+            importProvider.openProject(virtualFile, projectToClose, forceOpenInNewFrame)
+        }
+    }
 
-  override fun doOpenProject(virtualFile: VirtualFile, projectToClose: Project?, forceOpenInNewFrame: Boolean): Project? {
-    return runUnderModalProgressIfIsEdt {importProvider.openProject(virtualFile, projectToClose, forceOpenInNewFrame)}
-  }
+    override val name = GMavenConstants.GMAVEN
 
-  override val name = GMavenConstants.GMAVEN
+    override val icon = OpenapiIcons.RepositoryLibraryLogo
 
-  override val icon = OpenapiIcons.RepositoryLibraryLogo
+    override fun canImportProjectAfterwards(): Boolean = true
 
-  override fun canImportProjectAfterwards(): Boolean = true
-
-  override fun importProjectAfterwards(project: Project, file: VirtualFile) {
-    importProvider.linkToExistingProject(file, project)
-  }
+    override fun importProjectAfterwards(project: Project, file: VirtualFile) {
+        importProvider.linkToExistingProject(file, project)
+    }
 }
