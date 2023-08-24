c8a267d4343a51bb905037373e6ea3806370ee65
gmyasoedov <gmyasoedov@rtc.usetech.ru>
2023-08-24 16:24:31 +0300

    update intellij gradle plugin versions

--- gmaven/build.gradle.kts
+++ gmaven/build.gradle.kts
@@ -1,7 +1,7 @@
 plugins {
     id("java")
-    id("org.jetbrains.kotlin.jvm") version "1.6.20"
-    id("org.jetbrains.intellij") version "1.9.0"
+    id("org.jetbrains.kotlin.jvm") version "1.8.22"
+    id("org.jetbrains.intellij") version "1.14.1"
     id("org.jetbrains.changelog") version "2.1.0"
 }
 
