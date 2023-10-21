8c9d90f1c54d1c6a86fc28dcfdbf21c270c39bf4
gmyasoedov <gmyasoedov@rtc.usetech.ru>
2023-10-21 18:12:49 +0300

    chore: pom.xml icon

---
+++ gmaven/src/main/kotlin/ru/rzn/gmyasoedov/gmaven/ui/MavenIconProvider.kt
@@ -0,0 +1,18 @@
+package ru.rzn.gmyasoedov.gmaven.ui
+
+import com.intellij.ide.FileIconProvider
+import com.intellij.openapi.project.DumbAware
+import com.intellij.openapi.project.Project
+import com.intellij.openapi.vfs.VirtualFile
+import icons.OpenapiIcons.RepositoryLibraryLogo
+import ru.rzn.gmyasoedov.gmaven.util.CachedModuleData
+import javax.swing.Icon
+
+class MavenIconProvider : DumbAware, FileIconProvider {
+
+    override fun getIcon(file: VirtualFile, flags: Int, project: Project?): Icon? {
+        project ?: return null
+        val path = file.toNioPath().toString()
+        return if (CachedModuleData.getAllConfigPaths(project).contains(path)) RepositoryLibraryLogo else null
+    }
+}
