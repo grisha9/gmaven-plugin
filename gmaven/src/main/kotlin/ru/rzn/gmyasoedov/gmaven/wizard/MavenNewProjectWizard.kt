862ee6a74219227241e1ef38b7622c3c7e99b31f
gmyasoedov <gmyasoedov@rtc.usetech.ru>
2023-10-27 10:27:40 +0300

    fix: wizard module creation - relative new module path

--- gmaven/src/main/kotlin/ru/rzn/gmyasoedov/gmaven/wizard/MavenNewProjectWizard.kt
+++ gmaven/src/main/kotlin/ru/rzn/gmyasoedov/gmaven/wizard/MavenNewProjectWizard.kt
@@ -62,7 +62,7 @@ class MavenNewProjectWizard : BuildSystemJavaNewProjectWizard {
             var buildFile: VirtualFile? = null
             runWriteAction {
                 val moduleDir = Path.of(parentStep.path, parentStep.name)
-                buildFile = GMavenModuleBuilderHelper.createExternalProjectConfigFile(moduleDir)
+                buildFile = GMavenModuleBuilderHelper.createExternalProjectConfigFile(moduleDir, addSampleCode)
             }
 
             MavenUtils.runWhenInitialized(project) {
