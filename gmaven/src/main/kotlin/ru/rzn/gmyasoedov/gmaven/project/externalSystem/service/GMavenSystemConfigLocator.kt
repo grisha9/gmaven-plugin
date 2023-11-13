f4e4cabc83e0af5bd186c8384978eb9d1fbe33df
gmyasoedov <gmyasoedov@rtc.usetech.ru>
2023-11-13 13:43:05 +0300

    chore: added system config locator

---
+++ gmaven/src/main/kotlin/ru/rzn/gmyasoedov/gmaven/project/externalSystem/service/GMavenSystemConfigLocator.kt
@@ -0,0 +1,32 @@
+package ru.rzn.gmyasoedov.gmaven.project.externalSystem.service
+
+import com.intellij.openapi.externalSystem.service.settings.ExternalSystemConfigLocator
+import com.intellij.openapi.externalSystem.settings.ExternalProjectSettings
+import com.intellij.openapi.vfs.VirtualFile
+import com.intellij.openapi.vfs.readText
+import ru.rzn.gmyasoedov.gmaven.GMavenConstants
+import ru.rzn.gmyasoedov.gmaven.utils.MavenArtifactUtil.ARTIFACT_ID
+import ru.rzn.gmyasoedov.gmaven.utils.MavenUtils
+
+class GMavenSystemConfigLocator : ExternalSystemConfigLocator {
+    override fun getTargetExternalSystemId() = GMavenConstants.SYSTEM_ID
+
+    override fun adjust(configPath: VirtualFile): VirtualFile? {
+        if (!configPath.isDirectory) return configPath
+
+        val result = configPath.findChild(GMavenConstants.POM_XML)
+        if (result != null) return result
+
+        val configFileExtensions = MavenUtils.getConfigFileExtensions().toSet()
+        for (child in configPath.children) {
+            if (child.isDirectory) continue
+            val extension = child.extension ?: continue
+            if (configFileExtensions.contains(extension) && child.readText().contains(ARTIFACT_ID, true)) {
+                return child
+            }
+        }
+        return null
+    }
+
+    override fun findAll(externalProjectSettings: ExternalProjectSettings) = emptyList<VirtualFile>()
+}
