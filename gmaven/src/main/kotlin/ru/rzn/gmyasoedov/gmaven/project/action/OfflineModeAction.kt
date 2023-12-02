311baaa3b98017c01e21126fb5e12fd4a3cd607b
gmyasoedov <gmyasoedov@rtc.usetech.ru>
2023-12-02 17:56:23 +0300

    fix: gmaven tool window actions visibility

--- gmaven/src/main/kotlin/ru/rzn/gmyasoedov/gmaven/project/action/OfflineModeAction.kt
+++ gmaven/src/main/kotlin/ru/rzn/gmyasoedov/gmaven/project/action/OfflineModeAction.kt
@@ -4,6 +4,8 @@ import com.intellij.icons.AllIcons
 import com.intellij.openapi.actionSystem.ActionUpdateThread
 import com.intellij.openapi.actionSystem.AnActionEvent
 import com.intellij.openapi.actionSystem.ToggleAction
+import com.intellij.openapi.externalSystem.model.ExternalSystemDataKeys.EXTERNAL_SYSTEM_ID
+import ru.rzn.gmyasoedov.gmaven.GMavenConstants
 import ru.rzn.gmyasoedov.gmaven.bundle.GBundle
 import ru.rzn.gmyasoedov.gmaven.settings.MavenSettings
 
@@ -22,6 +24,11 @@ class OfflineModeAction : ToggleAction() {
 
     override fun getActionUpdateThread(): ActionUpdateThread {
         return ActionUpdateThread.EDT
+    }
+
+    override fun update(e: AnActionEvent) {
+        super.update(e)
+        e.presentation.isEnabledAndVisible = (e.getData(EXTERNAL_SYSTEM_ID) == GMavenConstants.SYSTEM_ID)
     }
 
     init {
