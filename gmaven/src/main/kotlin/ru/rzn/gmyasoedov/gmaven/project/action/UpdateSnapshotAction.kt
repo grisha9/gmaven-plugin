738d75c4f381771152707b1efde5acbf3aa9ae5f
gmyasoedov <gmyasoedov@rtc.usetech.ru>
2023-12-02 19:22:29 +0300

    fix: update snapshot action state

--- gmaven/src/main/kotlin/ru/rzn/gmyasoedov/gmaven/project/action/UpdateSnapshotAction.kt
+++ gmaven/src/main/kotlin/ru/rzn/gmyasoedov/gmaven/project/action/UpdateSnapshotAction.kt
@@ -3,10 +3,10 @@ package ru.rzn.gmyasoedov.gmaven.project.action
 import com.intellij.icons.AllIcons
 import com.intellij.openapi.actionSystem.ActionUpdateThread
 import com.intellij.openapi.actionSystem.AnActionEvent
+import com.intellij.openapi.actionSystem.Presentation
 import com.intellij.openapi.actionSystem.ToggleAction
 import com.intellij.openapi.externalSystem.model.ExternalSystemDataKeys.EXTERNAL_SYSTEM_ID
 import ru.rzn.gmyasoedov.gmaven.GMavenConstants
-import ru.rzn.gmyasoedov.gmaven.bundle.GBundle
 import ru.rzn.gmyasoedov.gmaven.settings.MavenSettings
 import ru.rzn.gmyasoedov.gmaven.settings.ProjectSettingsControlBuilder.SnapshotUpdateType
 
@@ -15,7 +15,7 @@ class UpdateSnapshotAction : ToggleAction() {
     override fun isSelected(e: AnActionEvent): Boolean {
         val project = e.project ?: return false
         val settings = MavenSettings.getInstance(project).linkedProjectsSettings.firstOrNull() ?: return false
-        updateState(settings.snapshotUpdateType)
+        updateState(settings.snapshotUpdateType, e.presentation)
         return settings.snapshotUpdateType != SnapshotUpdateType.DEFAULT
     }
 
@@ -26,7 +26,7 @@ class UpdateSnapshotAction : ToggleAction() {
         val ordinal = currentType.ordinal
         val next = SnapshotUpdateType.values()[(ordinal + 1) % SnapshotUpdateType.values().size]
         MavenSettings.getInstance(project).linkedProjectsSettings.forEach { it.snapshotUpdateType = next }
-        updateState(next)
+        updateState(next, e.presentation)
     }
 
     override fun getActionUpdateThread(): ActionUpdateThread {
@@ -38,15 +38,20 @@ class UpdateSnapshotAction : ToggleAction() {
         e.presentation.isEnabledAndVisible = (e.getData(EXTERNAL_SYSTEM_ID) == GMavenConstants.SYSTEM_ID)
     }
 
-    init {
-        templatePresentation.text = GBundle.message("gmaven.action.snapshot")
-    }
-
-    private fun updateState(type: SnapshotUpdateType) {
+    private fun updateState(type: SnapshotUpdateType, presentation: Presentation) {
         when (type) {
-            SnapshotUpdateType.DEFAULT -> templatePresentation.icon = AllIcons.Actions.FindEntireFile
-            SnapshotUpdateType.NEVER -> templatePresentation.icon = AllIcons.General.InspectionsTrafficOff
-            else -> templatePresentation.icon = AllIcons.Actions.ForceRefresh
+            SnapshotUpdateType.DEFAULT -> {
+                presentation.icon = AllIcons.Actions.FindEntireFile
+                presentation.text = "Update Snapshots: Default"
+            }
+            SnapshotUpdateType.NEVER -> {
+                presentation.icon = AllIcons.General.InspectionsTrafficOff
+                presentation.text = "Update Snapshots: Never (-nsu)"
+            }
+            else -> {
+                presentation.icon = AllIcons.Actions.ForceRefresh
+                presentation.text = "Update Snapshots: Force (-U)"
+            }
         }
     }
 }
