9743f91b4bf244d73dd534e0d0359769842e637b
gmyasoedov <gmyasoedov@rtc.usetech.ru>
2023-12-17 13:48:00 +0300

    fix: dependency pom navigation

--- gmaven/src/main/kotlin/ru/rzn/gmyasoedov/gmaven/dom/MavenArtifactIdXmlPsiReferenceContributor.kt
+++ gmaven/src/main/kotlin/ru/rzn/gmyasoedov/gmaven/dom/MavenArtifactIdXmlPsiReferenceContributor.kt
@@ -1,5 +1,9 @@
 package ru.rzn.gmyasoedov.gmaven.dom
 
+import com.intellij.openapi.externalSystem.model.ProjectKeys
+import com.intellij.openapi.externalSystem.service.project.ProjectDataManager
+import com.intellij.openapi.externalSystem.util.ExternalSystemApiUtil
+import com.intellij.openapi.project.Project
 import com.intellij.openapi.util.TextRange
 import com.intellij.openapi.vfs.LocalFileSystem
 import com.intellij.patterns.XmlPatterns
@@ -8,6 +12,7 @@ import com.intellij.psi.PsiReferenceBase.Immediate
 import com.intellij.psi.xml.XmlFile
 import com.intellij.psi.xml.XmlTag
 import com.intellij.util.ProcessingContext
+import ru.rzn.gmyasoedov.gmaven.GMavenConstants
 import ru.rzn.gmyasoedov.gmaven.settings.MavenSettings
 import ru.rzn.gmyasoedov.gmaven.util.CachedModuleData
 import ru.rzn.gmyasoedov.gmaven.util.CachedModuleDataService
@@ -46,27 +51,6 @@ private class MavenArtifactIdXmlPsiReferenceProvider : PsiReferenceProvider() {
         }
     }
 
-    private fun getLocalRepositoryPsiReferences(
-        element: XmlTag, xmlTag: XmlTag, value: String, range: TextRange
-    ): Array<PsiReference> {
-        val path = element.containingFile.parent?.virtualFile?.canonicalPath ?: return emptyArray()
-        val localRepositoryPath = MavenSettings.getInstance(element.project)
-            .getLinkedProjectSettings(path)?.localRepositoryPath ?: return emptyArray()
-        val parentXml = xmlTag.parentTag ?: return emptyArray()
-        val groupId = parentXml.getSubTagText(MavenArtifactUtil.GROUP_ID) ?: return emptyArray()
-        val version = parentXml.getSubTagText(MavenArtifactUtil.VERSION) ?: return emptyArray()
-        return try {
-            val artifactNioPathPom = MavenArtifactUtil
-                .getArtifactNioPathPom(Path(localRepositoryPath), groupId, value, version)
-            val virtualFile = LocalFileSystem.getInstance()
-                .findFileByNioFile(artifactNioPathPom) ?: return emptyArray()
-            val psiFile = PsiManager.getInstance(element.project).findFile(virtualFile) ?: return emptyArray()
-            arrayOf(Immediate(xmlTag, range, true, psiFile))
-        } catch (e: Exception) {
-            emptyArray()
-        }
-    }
-
     private fun getModulePsiReferences(
         moduleData: CachedModuleData, element: XmlTag, xmlTag: XmlTag, range: TextRange
     ): Array<PsiReference> {
@@ -76,5 +60,54 @@ private class MavenArtifactIdXmlPsiReferenceProvider : PsiReferenceProvider() {
         val artifactIdTag = psiFile.rootTag?.subTags
             ?.find { it.name == MavenArtifactUtil.ARTIFACT_ID } ?: return emptyArray()
         return arrayOf(Immediate(xmlTag, range, true, artifactIdTag))
+    }
+
+    private fun getLocalRepositoryPsiReferences(
+        element: XmlTag, xmlTag: XmlTag, artifactId: String, range: TextRange
+    ): Array<PsiReference> {
+        val path = element.containingFile.parent?.virtualFile?.canonicalPath ?: return emptyArray()
+        val settings = MavenSettings.getInstance(element.project).getLinkedProjectSettings(path) ?: return emptyArray()
+        val localRepositoryPath = settings.localRepositoryPath ?: return emptyArray()
+        val parentXml = xmlTag.parentTag ?: return emptyArray()
+        val groupId = parentXml.getSubTagText(MavenArtifactUtil.GROUP_ID) ?: return emptyArray()
+        val version = getDependencyVersion(parentXml, groupId, artifactId, settings.externalProjectPath)
+            ?: return emptyArray()
+        //val version = parentXml.getSubTagText(MavenArtifactUtil.VERSION) ?: return emptyArray()
+        return try {
+            val artifactNioPathPom = MavenArtifactUtil
+                .getArtifactNioPathPom(Path(localRepositoryPath), groupId, artifactId, version)
+            val virtualFile = LocalFileSystem.getInstance()
+                .findFileByNioFile(artifactNioPathPom) ?: return emptyArray()
+            val psiFile = PsiManager.getInstance(element.project).findFile(virtualFile) ?: return emptyArray()
+            arrayOf(Immediate(xmlTag, range, true, psiFile))
+        } catch (e: Exception) {
+            emptyArray()
+        }
+    }
+
+    private fun getDependencyVersion(
+        parentXml: XmlTag, groupId: String, artifactId: String, projectPath: String
+    ): String? {
+        val version = parentXml.getSubTagText(MavenArtifactUtil.VERSION)
+        if (version == null) {
+            val moduleName = (parentXml.containingFile as? XmlFile)?.rootTag
+                ?.getSubTagText(MavenArtifactUtil.ARTIFACT_ID) ?: return null
+            return getVersionFromProjectStructure(parentXml.project, moduleName, groupId, artifactId, projectPath)
+        }
+        return version
+    }
+
+    private fun getVersionFromProjectStructure(
+        project: Project, moduleName: String, groupId: String, artifactId: String, projectPath: String
+    ): String? {
+        val projectStructure = ProjectDataManager.getInstance()
+            .getExternalProjectData(project, GMavenConstants.SYSTEM_ID, projectPath)
+            ?.externalProjectStructure ?: return null
+
+        val moduleNode = ExternalSystemApiUtil.findAll(projectStructure, ProjectKeys.MODULE)
+            .find { it.data.moduleName == moduleName } ?: return null
+        return ExternalSystemApiUtil.findAll(moduleNode, ProjectKeys.LIBRARY_DEPENDENCY)
+            .find { it.data.target.artifactId == artifactId && it.data.target.groupId == groupId }
+            ?.data?.target?.version
     }
 }
