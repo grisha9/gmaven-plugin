745ea953ef141139780ee24d7ca3c9a33e27c151
gmyasoedov <gmyasoedov@rtc.usetech.ru>
2024-03-08 13:18:08 +0300

    chore: improve xml dom gutter
    - find artifact from dependency management and plugin management sections
    - xml references for artifactId

--- gmaven/src/main/kotlin/ru/rzn/gmyasoedov/gmaven/dom/GMavenSchemaProvider.kt
+++ gmaven/src/main/kotlin/ru/rzn/gmyasoedov/gmaven/dom/GMavenSchemaProvider.kt
@@ -3,16 +3,15 @@ package ru.rzn.gmyasoedov.gmaven.dom
 import com.intellij.javaee.ResourceRegistrar
 import com.intellij.javaee.StandardResourceProvider
 import ru.rzn.gmyasoedov.gmaven.utils.MavenUtils
-import java.util.Map
 
 class GMavenSchemaProvider : StandardResourceProvider {
     override fun registerResources(registrar: ResourceRegistrar?) {
         if (MavenUtils.pluginEnabled(MavenUtils.INTELLIJ_MAVEN_PLUGIN_ID) || registrar == null) return
 
-        Map.of(
-            "http://maven.apache.org/xsd/maven-4.0.0.xsd", "/schemas/maven-4.0.0.xsd",
-            "http://maven.apache.org/maven-v4_0_0.xsd", "/schemas/maven-4.0.0.xsd",
-            "http://maven.apache.org/xsd/settings-1.2.0.xsd", "/schemas/settings-1.2.0.xsd",
+        mutableMapOf(
+            "http://maven.apache.org/xsd/maven-4.0.0.xsd" to "/schemas/maven-4.0.0.xsd",
+            "http://maven.apache.org/maven-v4_0_0.xsd" to "/schemas/maven-4.0.0.xsd",
+            "http://maven.apache.org/xsd/settings-1.2.0.xsd" to "/schemas/settings-1.2.0.xsd",
         ).forEach { (url, path) -> addStdResource(registrar, url, path) }
     }
 
