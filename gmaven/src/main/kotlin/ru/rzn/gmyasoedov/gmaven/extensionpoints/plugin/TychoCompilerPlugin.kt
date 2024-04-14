aee885bba3df64e3e8d3838a42908e983562bdac
gmyasoedov <gmyasoedov@rtc.usetech.ru>
2024-04-14 12:40:13 +0300

    chore: base support tycho-compiler-plugin

---
+++ gmaven/src/main/kotlin/ru/rzn/gmyasoedov/gmaven/extensionpoints/plugin/TychoCompilerPlugin.kt
@@ -0,0 +1,34 @@
+package ru.rzn.gmyasoedov.gmaven.extensionpoints.plugin
+
+import org.jdom.Element
+import ru.rzn.gmyasoedov.gmaven.project.externalSystem.model.MainJavaCompilerData
+import ru.rzn.gmyasoedov.serverapi.model.MavenPlugin
+import ru.rzn.gmyasoedov.serverapi.model.MavenProject
+import java.nio.file.Path
+
+class TychoCompilerPlugin : MavenCompilerFullImportPlugin {
+    override fun getGroupId() = "org.eclipse.tycho"
+
+    override fun getArtifactId() = "tycho-compiler-plugin"
+
+    override fun priority() = 20
+
+    override fun getCompilerData(
+        project: MavenProject,
+        plugin: MavenPlugin,
+        localRepositoryPath: Path,
+        contextElementMap: MutableMap<String, Element>
+    ): CompilerData {
+        return ApacheMavenCompilerPlugin().getCompilerData(project, plugin, localRepositoryPath, contextElementMap)
+    }
+
+    override fun getJavaCompilerData(
+        project: MavenProject,
+        plugin: MavenPlugin,
+        compilerData: CompilerData,
+        localRepositoryPath: Path,
+        contextElementMap: MutableMap<String, Element>
+    ): MainJavaCompilerData {
+        return MainJavaCompilerData.createDefault()
+    }
+}
