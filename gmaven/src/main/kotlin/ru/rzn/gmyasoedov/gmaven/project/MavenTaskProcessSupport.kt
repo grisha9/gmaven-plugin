da0a4a7e72ab1ca1abc393a1d2a8e218fb572b13
gmyasoedov <gmyasoedov@yandex.ru>
2024-08-18 18:18:30 +0300

    chore: way to standalone maven-model-reader-plugin. part2. import process

--- gmaven/src/main/kotlin/ru/rzn/gmyasoedov/gmaven/project/MavenTaskProcessSupport.kt
+++ gmaven/src/main/kotlin/ru/rzn/gmyasoedov/gmaven/project/MavenTaskProcessSupport.kt
@@ -1,6 +1,7 @@
 package ru.rzn.gmyasoedov.gmaven.project
 
 import com.intellij.execution.configurations.GeneralCommandLine
+import com.intellij.openapi.util.SystemInfo
 import com.intellij.util.execution.ParametersListUtil
 import ru.rzn.gmyasoedov.gmaven.extensionpoints.plugin.MavenCompilerFullImportPlugin
 import ru.rzn.gmyasoedov.gmaven.extensionpoints.plugin.MavenFullImportPlugin
@@ -8,26 +9,53 @@ import ru.rzn.gmyasoedov.gmaven.server.GServerRequest
 import ru.rzn.gmyasoedov.gmaven.server.MavenServerCmdState
 import java.nio.file.Path
 import kotlin.io.path.absolutePathString
+import kotlin.io.path.exists
+import kotlin.io.path.name
 
 
 class MavenTaskProcessSupport(private val request: GServerRequest, private val isImport: Boolean = true) {
     private val workingDirectory = getWorkingDirectory(request)
-//QualityToolProcessHandler
+
+    //QualityToolProcessHandler
     fun getCommandLine(): GeneralCommandLine {
         val commandLine = GeneralCommandLine()
         setupDebugParam(commandLine)
         setupGmavenPluginsProperty(commandLine)
         setupMavenOpts(commandLine)
-        commandLine.exePath = request.mavenPath.resolve("bin").resolve("mvn").absolutePathString()
-        commandLine.addParameter("ru.rzn.gmyasoedov:maven-model-reader-plugin:1.0-SNAPSHOT:resolve")
+        commandLine.exePath = getExeMavenPath()
+
         commandLine.workDirectory = workingDirectory.toFile()
         commandLine.isRedirectErrorStream = true
 
+        /*commandLine.addParameter("ru.rzn.gmyasoedov:maven-model-reader-plugin:1.0-SNAPSHOT:resolve")
         commandLine.addParameter("-f")
-        commandLine.addParameter(workingDirectory.absolutePathString())
-        commandLine.environment["JAVA_HOME"] = request.settings.javaHome
+        commandLine.addParameter(workingDirectory.absolutePathString())*/
         commandLine.addParameter("-DresultAsTree=true")
+        commandLine.environment["JAVA_HOME"] = request.settings.javaHome
         return commandLine
+    }
+
+    private fun getExeMavenPath(): String {
+        val mavenPath = request.mavenPath
+        if (mavenPath.name == "mvnw" || mavenPath.name == "mvnw.cmd") return mavenPath.absolutePathString()
+        if (mavenPath.name == "mvnd.sh" || mavenPath.name == "mvnd.cmd") return mavenPath.absolutePathString()
+        return if (SystemInfo.isWindows) getWinPath(mavenPath) else getUnixPath(mavenPath)
+    }
+
+    private fun getUnixPath(mavenPath: Path): String {
+        val basePath = mavenPath.resolve("bin")
+        if (basePath.resolve("mvn").exists()) {
+            return basePath.resolve("mvn").absolutePathString()
+        }
+        return basePath.resolve("mvn.sh").absolutePathString()
+    }
+
+    private fun getWinPath(mavenPath: Path): String {
+        val basePath = mavenPath.resolve("bin")
+        if (basePath.resolve("mvn.cmd").exists()) {
+            return basePath.resolve("mvn.cmd").absolutePathString()
+        }
+        return basePath.resolve("mvn.bat").absolutePathString()
     }
 
     private fun setupDebugParam(commandLine: GeneralCommandLine) {
@@ -37,6 +65,7 @@ class MavenTaskProcessSupport(private val request: GServerRequest, private val i
     }
 
     private fun setupGmavenPluginsProperty(params: GeneralCommandLine) {
+        if (!isImport) return
         val extensionList = MavenFullImportPlugin.EP_NAME.extensionList
         val pluginsForImport: MutableList<String> = ArrayList(extensionList.size)
         val pluginsForResolve: MutableList<String> = ArrayList(extensionList.size)
