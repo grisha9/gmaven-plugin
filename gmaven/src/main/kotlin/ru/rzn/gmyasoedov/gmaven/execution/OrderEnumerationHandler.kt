38917c521cc234c6f0c4f672f72032a45b147f94
gmyasoedov <gmyasoedov@yandex.ru>
2025-02-21 20:05:02 +0300

    fix: EasyMaven ignores dependency version override with provided scope #27

---
+++ gmaven/src/main/kotlin/ru/rzn/gmyasoedov/gmaven/execution/OrderEnumerationHandler.kt
@@ -0,0 +1,23 @@
+package ru.rzn.gmyasoedov.gmaven.execution
+
+import com.intellij.openapi.externalSystem.util.ExternalSystemApiUtil
+import com.intellij.openapi.roots.OrderEnumerationHandler
+import ru.rzn.gmyasoedov.gmaven.GMavenConstants
+
+object MavenOrderEnumerationHandler : OrderEnumerationHandler() {
+
+    open class Factory : OrderEnumerationHandler.Factory() {
+        override fun isApplicable(module: Module) = ExternalSystemApiUtil
+            .isExternalSystemAwareModule(GMavenConstants.SYSTEM_ID, module)
+
+        override fun createHandler(module: Module) = MavenOrderEnumerationHandler
+    }
+
+    override fun shouldAddRuntimeDependenciesToTestCompilationClasspath() = true
+
+    override fun shouldIncludeTestsFromDependentModulesToTestClasspath() = false
+
+    override fun shouldProcessDependenciesRecursively() = false
+
+    override fun areResourceFilesFromSourceRootsCopiedToOutput() = false
+}
