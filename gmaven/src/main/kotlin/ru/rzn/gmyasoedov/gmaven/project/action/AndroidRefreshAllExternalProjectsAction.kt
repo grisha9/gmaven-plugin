42ee3594e9cbdc317d08b99382c20dd9407ca54f
gmyasoedov <gmyasoedov@yandex.ru>
2025-06-28 11:15:00 +0300

    fix: attach source #34 support android studio meerkat
    https://github.com/grisha9/gmaven-plugin/issues/34

---
+++ gmaven/src/main/kotlin/ru/rzn/gmyasoedov/gmaven/project/action/AndroidRefreshAllExternalProjectsAction.kt
@@ -0,0 +1,35 @@
+package ru.rzn.gmyasoedov.gmaven.project.action
+
+import com.intellij.openapi.actionSystem.ActionUpdateThread
+import com.intellij.openapi.actionSystem.AnActionEvent
+import com.intellij.openapi.application.ApplicationNamesInfo
+import com.intellij.openapi.externalSystem.importing.ImportSpecBuilder
+import com.intellij.openapi.externalSystem.util.ExternalSystemBundle
+import com.intellij.openapi.externalSystem.util.ExternalSystemUtil
+import com.intellij.openapi.project.DumbAwareAction
+import ru.rzn.gmyasoedov.gmaven.GMavenConstants.SYSTEM_ID
+
+class AndroidRefreshAllExternalProjectsAction : DumbAwareAction() {
+    init {
+        templatePresentation.setText(
+            ExternalSystemBundle.messagePointer("action.refresh.all.projects.text", SYSTEM_ID.readableName)
+        )
+        templatePresentation.setDescription(
+            ExternalSystemBundle.messagePointer("action.refresh.all.projects.description", SYSTEM_ID.readableName)
+        )
+    }
+
+    override fun getActionUpdateThread() = ActionUpdateThread.BGT
+
+    override fun update(e: AnActionEvent) {
+        super.update(e)
+        val fullProductName = ApplicationNamesInfo.getInstance().fullProductName
+        e.presentation.isEnabledAndVisible = fullProductName.contains("android", true)
+    }
+
+    override fun actionPerformed(e: AnActionEvent) {
+        val project = e.project ?: return
+        ExternalSystemUtil.refreshProjects(ImportSpecBuilder(project, SYSTEM_ID))
+    }
+
+}
